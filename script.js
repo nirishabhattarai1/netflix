b0188132f9af73d3fc2c9798bb80977863421758
nirishabhattarai1 <nirishabhattarai42@gmail.com>
2023-07-12 21:44:36 +0545

    first project

---
+++ script.js
@@ -0,0 +1,63 @@
+const hello=document.getElementById('paragraph');
+
+function geet() {
+    if(hello.style.display != 'none'){
+        hello.style.display = 'none'; 
+    }
+    else{
+        hello.style.display = 'block';
+    }
+}
+
+const helloo=document.getElementById('paragraphh');
+
+function geeet() {
+    if(helloo.style.display != 'none'){
+        helloo.style.display = 'none'; 
+    }
+    else{
+        helloo.style.display = 'block';
+    }
+}
+
+const hello2=document.getElementById('paragraph2');
+
+function geet2() {
+    if(hello2.style.display != 'none'){
+        hello2.style.display = 'none'; 
+    }
+    else{
+        hello2.style.display = 'block';
+    }
+}
+
+const hello3=document.getElementById('paragraph3');
+
+function geet3() {
+    if(hello3.style.display != 'none'){
+        hello3.style.display = 'none'; 
+    }
+    else{
+        hello3.style.display = 'block';
+    }
+}
+const hello4=document.getElementById('paragraph4');
+
+function geet4() {
+    if(hello4.style.display != 'none'){
+        hello4.style.display = 'none'; 
+    }
+    else{
+        hello4.style.display = 'block';
+    }
+}
+const hello5=document.getElementById('paragraph5');
+
+function geet5() {
+    if(hello5.style.display != 'none'){
+        hello5.style.display = 'none'; 
+    }
+    else{
+        hello5.style.display = 'block';
+    }
+}
